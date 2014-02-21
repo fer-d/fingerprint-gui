af72ba3ff65b34219c560d5e8ccfb6386ec27199
fer-d <fer_d@saifas.com>
2014-02-20 22:40:25 -0600

    [refs #1] Add packages from documentation

---
+++ install-dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get install libfprint-dev libqt4-dev   \
+                     libqca2-dev libfakekey-dev \
+                     libusb-1.0-0-dev 		\
+                     libpolkit-qt-1-dev libpam-dev
