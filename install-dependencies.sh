27238d39819554ce594ca91244d5a71b4b82a225
fer-d <fer_d@saifas.com>
2014-02-21 06:37:41 +0400

    [refs #1] Add g++ to installation list

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install libfprint-dev libqt4-dev   \
                      libqca2-dev libfakekey-dev \
-                     libusb-1.0-0-dev 		\
+                     libusb-1.0-0-dev g++       \
                      libpolkit-qt-1-dev libpam-dev
